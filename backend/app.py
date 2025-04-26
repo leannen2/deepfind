9d96601cf51849f1594f9fe32baccdad59b7bac2
leannen2 <leannen2@uci.edu>
2025-04-26 11:24:22 -0700

    change port

--- backend/app.py
+++ backend/app.py
@@ -52,4 +52,4 @@ def find():
         return jsonify({'error': f'Exception: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
