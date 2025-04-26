f412f0c60c3d04dac6e853e694c70abdb50b2c82
Liliana Nguyen <liliananguyen03@gmail.com>
2025-04-26 09:58:06 -0700

    ln - full implementation of semantic find

--- frontend/content.js
+++ frontend/content.js
@@ -37,7 +37,7 @@ function markAllTerms(terms) {
     done: () => {
       Promise.all(terms.map((term) => markTerm(term))).then(() => {
         results = Array.from(document.body.querySelectorAll("mark"));
-        console.log(results);
+        // console.log(results);
         currentIndex = 0;
         jumpTo();
       });
@@ -45,31 +45,59 @@ function markAllTerms(terms) {
   });
 }
 
+// Send HTML/PDF content and query to backend
+async function getListOfSemanticMatches(contentType, query) {
+  try {
+    const html_content = document.documentElement.outerHTML;
+    const res = await fetch('http://localhost:5002/api/find', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: html_content,
+        contentType: contentType,
+        query: query,
+      }),
+    });
+    const response = await res.json();
+    console.log("similar_terms", response.similar_terms)
+    return response.similar_terms; 
+  } catch (error) {
+    console.error('Failed to get list of semantic matches:', error);
+  }
+}
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "markPage") {
-    markAllTerms(message.markList);
+    
     console.log("message received");
     
-    fetch("http://127.0.0.1:5001/api/dummy")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("API response from content.js:", data);
-    })
-    .catch((err) => {
-      console.error("API error in content.js:", err);
-    });
+    getListOfSemanticMatches('html', message.query)
+      .then((markList) => {
+        if (markList && Array.isArray(markList)) {
+          markAllTerms(markList);
+        } else {
+          console.warn("No terms received to mark.");
+        }
+      })
+      .catch((err) => {
+        console.error("Error getting semantic matches:", err);
+      });
     sendResponse({ status: "ok" });
     return true;
   } else if (message.action === "jumpForward") {
-    currentIndex++;
+    currentIndex = (currentIndex + 1) % results.length;
     jumpTo();
     sendResponse({ status: "ok" });
     return true;
   } else if (message.action === "jumpBackward") {
-    currentIndex--;
+    currentIndex = (currentIndex - 1 + results.length) % results.length;
     jumpTo();
     sendResponse({ status: "ok" });
     return true;
   }
 });
+
+
+
